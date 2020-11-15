645e0996515a5be8c8d5a0671353724b4bf94c4b
nabilla <nabilla@avana.id>
2020-11-15 22:31:08 +0700

    add comment on LearnUnitTestingTests

--- LearnUnitTestingTests/LearnUnitTestingTests.swift
+++ LearnUnitTestingTests/LearnUnitTestingTests.swift
@@ -11,19 +11,31 @@ import XCTest
 
 class LearnUnitTestingTests: XCTestCase {
 
+    // REQUIREMENT:
+    // Naming: func test<System Under Test>_<Conditiion or State Change>_<Expected Result>()
+    // Ex: func testSignUpModel_WhenInformationProvided_PasswordShouldMatchRepeatPassword()
+    // will help to know the function of test class with only see the name of function:)
+
+    // This code only called one
+    // ex: you can create instance of your class in here
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    // To remove created instance, this is will help the code to removed after we're done to testing it
+    // will make app faster too
+    // running after all test case already executed.
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    // example of testing class
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    // to testing perfomance of test case class
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
